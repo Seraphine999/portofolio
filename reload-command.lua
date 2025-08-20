b3a88cc618520441468eece44dfc585af724549d
Seraphine999 <dzakymahardika999@gmail.com>
2025-08-20 21:45:36 +0700

    Update reload-command.lua

--- reload-command.lua
+++ reload-command.lua
@@ -1,24 +1,25 @@
--- Reload Scripts script
+  -- Reload Scripts script
 print("(Loaded) Reload Scripts for GrowSoft")
 
 Roles = {
-    ROLE_NONE = 0,
+    ROLE_PLAYER = 0,
     ROLE_VIP = 1,
     ROLE_MODERATOR = 2,
     ROLE_ADMIN = 3,
-    ROLE_OWNER = 4,
-    ROLE_CREATOR = 5,
+    ROLE_CHEATER = 4,
+    ROLE_OWNER = 5,
+    ROLE_CREATOR = 6,
 }
 
 local rsCommandData = {
     command = "rs",
-    roleRequired = Roles.ROLE_OWNER,
+    roleRequired = Roles.ROLE_CREATOR,
     description = "This command allows you to `$reload`` scripts."
 }
 
 local reloadScriptsCommandData = {
     command = "reloadscripts",
-    roleRequired = Roles.ROLE_OWNER,
+    roleRequired = Roles.ROLE_CREATOR,
     description = "This command allows you to `$reload`` scripts."
 }
 
